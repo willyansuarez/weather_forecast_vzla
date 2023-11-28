e35b65f747d3245967dda7e0aae09e4287b9bb75
willyansuarez <willyanjosesuarez@gmail.com>
2023-11-27 22:02:00 -0400

    Para probar si sirve secrets de github con el deploy en streamlit cloud

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # weather_forecast_vzla
 Ejemplo simple de acceso a una api del clima para obtener el pronóstico para el día actual
+
+**Sobre el proyecto**  
+El proyecto trata sobre el acceso a los datos del tiempo(pronóstico del clima)  
+a través de una api del servicio de weather api(https://www.weatherapi.com/)  
+
+Se importan algunas librerías para tal fin, como BeatifulSoup, json, pandas  
+entre otras, obtenemos los datos en formato json y este json se encuntra cargado  
+con toda la data necesaria para el pronóstico del clima para la ciudad elegida.
+
+El código no tiene mayores complicaciones y se aprende a buscar datos en las keys  
+de los json que a su vez en sus values, contienen muchos datos, lo cual confunde un  
+poco, pero una vez obtenido el primer dato, se comprende como obtener los demás.
+
+El proyecto era suscribirse a un servicio de mensajeria y que al usuario le  
+llegara un mensaje en la mañana por ejemplo a las 6:am con el pronóstico del día  
+pero se decide implementar en streamlit por varias razones:
+
+1- El servicio de mensajería no es gratuito, aunque se otorga un monto para pruebas  
+2- Mostrar la app en streamlit y desplegar en la web mediante streamlit cloud
+3- Subir el proyecto a github para mostrarlo y streamlit toma el proyecto desde  
+github
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import time
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
@@ -10,62 +11,83 @@ from datetime import datetime
 
 
 # print("todo bien")
-# print(API_KEY_WAPI)
-api_key = API_KEY_WAPI
+# st.write("Hola *mundo* :sunglasses:")
 
+# título de la app
+st.title("Pronóstico del Clima")
+
+# sidebar
+with st.sidebar:
+    ciudad = st.selectbox(
+        "Selecciona la ciudad",
+        ["Barquisimeto", "Maracay", "Caracas", "Valencia", "San Juan de los Morros"],
+        index = None,
+        placeholder = "Ciudad",)
+    # option = st.selectbox(
+    # 'How would you like to be contacted?',
+    # ('Email', 'Home phone', 'Mobile phone'))
+
+    # st.button("Ver pronóstico")
+
+st.write('Seleccionaste : ', ciudad)
+st.button("Ver pronóstico")
+
+# clave de api, cada clave de api es única, debe generarse su propia clave de api
+# api_key = API_KEY_WAPI
+api_key = ${{ secrets.settings }}
+
+# guardar la ciudad en la variable, podría ser una lista desplegable con
+# varias ciudades de Venezuela o del país de residencia
 query = "Barquisimeto"
 
+# # URL de la api
 weather_url = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+query+'&days=1&aqi=no&alerts=no'
+# weather_url = 'http://api.weatherapi.com/v1/forecast.json?key='+api_key+'&q='+ciudad+'&days=1&aqi=no&alerts=no'
 
 response = requests.get(weather_url).json()
-# print(response)
-
-# # obtener las keys del json
-# keys_response = response.keys()
-# print(keys_response)
-
 
 # obtener la longitud de las horas del día
 day_hours = len(response["forecast"]["forecastday"][0]["hour"])
-print(day_hours)
+st.write(day_hours)
 
-# obtener la fecha
-actual_date = response["forecast"]["forecastday"][0]["hour"][1]["time"].split()[0]
-print(actual_date)
+# # obtener la fecha
+# actual_date = response["forecast"]["forecastday"][0]["hour"][1]["time"].split()[0]
+# st.write(actual_date)
 
-# obtener la hora del día como número entero
-hour_day = int(response["forecast"]["forecastday"][0]["hour"][1]["time"].split()[1].split(":")[0])
-print(hour_day)
+# # obtener la hora del día como número entero
+# hour_day = int(response["forecast"]["forecastday"][0]["hour"][1]["time"].split()[1].split(":")[0])
+# st.write(hour_day)
 
-# obtener la condición
-condition = response["forecast"]["forecastday"][0]["hour"][0]["condition"]["text"]
-print(condition)
+# # obtener la condición
+# condition = response["forecast"]["forecastday"][0]["hour"][0]["condition"]["text"]
+# st.write(condition)
 
-# obtener la temperatura
-temperature = response["forecast"]["forecastday"][0]["hour"][0]["temp_c"]
-print(temperature)
+# # obtener la temperatura
+# temperature = response["forecast"]["forecastday"][0]["hour"][0]["temp_c"]
+# st.write(temperature)
 
-# obtener si lloverá o no: 0 = False, 1 = True
-it_will_rain = response["forecast"]["forecastday"][0]["hour"][0]["will_it_rain"]
-print(it_will_rain)
+# # obtener si lloverá o no: 0 = False, 1 = True
+# it_will_rain = response["forecast"]["forecastday"][0]["hour"][0]["will_it_rain"]
+# st.write(it_will_rain)
 
-# obtener la probabilidad de lluvia: un número entero hasta 100
-rain_prob = response["forecast"]["forecastday"][0]["hour"][0]["chance_of_rain"]
-print(rain_prob)
+# # obtener la probabilidad de lluvia: un número entero hasta 100
+# rain_prob = response["forecast"]["forecastday"][0]["hour"][0]["chance_of_rain"]
+# st.write(rain_prob)
+
 
 # función para obtener los datos de la api
 # def get_forecast(response, i):
-#     actual_date_f = response["forecast"]["forecastday"][0]["hour"][1]["time"].split()[0]
-#     hour_day_f = int(response["forecast"]["forecastday"][0]["hour"][1]["time"].split()[1].split(":")[0])
-#     condition_f = response["forecast"]["forecastday"][0]["hour"][0]["condition"]["text"]
-#     temperature_f = response["forecast"]["forecastday"][0]["hour"][0]["temp_c"]
-#     it_will_rain_f = response["forecast"]["forecastday"][0]["hour"][0]["will_it_rain"]
-#     rain_prob_f = response["forecast"]["forecastday"][0]["hour"][0]["chance_of_rain"]
+#     actual_date_f = response["forecast"]["forecastday"][0]["hour"][i]["time"].split()[0]
+#     hour_day_f = int(response["forecast"]["forecastday"][0]["hour"][i]["time"].split()[1].split(":")[0])
+#     condition_f = response["forecast"]["forecastday"][0]["hour"][i]["condition"]["text"]
+#     temperature_f = response["forecast"]["forecastday"][0]["hour"][i]["temp_c"]
+#     it_will_rain_f = response["forecast"]["forecastday"][0]["hour"][i]["will_it_rain"]
+#     rain_prob_f = response["forecast"]["forecastday"][0]["hour"][i]["chance_of_rain"]
 
 #     return actual_date_f, hour_day_f, condition_f, temperature_f, it_will_rain_f, rain_prob_f
 
 
-# # array vacío para llenar con el número de registros
+# # # array vacío para llenar con el número de registros
 # data = []
 
 # # ciclo para llenar el array vacío
@@ -78,13 +100,14 @@ print(rain_prob)
 
 # # crear el dataframe
 # df = pd.DataFrame(data, columns=cols)
+# df
 
 # # crear un dataframe filtrando los resultados por lluvia y hora
-
 # df_rain = df[(df["Lluvia"] == 1) & (df["Hora"] > 6) & (df["Hora"] < 22)]
 
 # df_rain = df_rain[["Hora", "Condición"]]
 # df_rain.set_index("Hora", inplace=True)
 
 # # mensaje para imprimir el pronóstico
-# print('\n¡Hola \n\n El Pronóstico del tiempo hoy ' + df["Fecha"][0] + ' en ' + query + ' es: \n\n ' + str(df_rain))
+# st.write('\n¡Hola \n\n El Pronóstico del tiempo hoy ' + df["Fecha"][0] + ' en ' + query + ' es: \n\n ')
+# df_rain
